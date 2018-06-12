5b62fc8364f7c042d9b82bb7dfd53597805c169d
randysuen1991 <randysuen1991@gmail.com>
2018-06-12 09:41:03 +0800

    new

---
+++ DataAnlysis.py
@@ -0,0 +1,7 @@
+from sklearn.decomposition import FactorAnalysis
+
+
+
+class FactorModel():
+    def __init__(self,n_components,**kwargs):
+        self.fa = FactorAnalysis(n_components=n_components)
