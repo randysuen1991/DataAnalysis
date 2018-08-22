de279463663b8282bd5eb8d072e7765b6561d772
randysuen1991 <randysuen1991@gmail.com>
2018-08-22 17:15:00 +0800

    new

--- DataHandler.py
+++ DataHandler.py
@@ -1,26 +1,38 @@
 import pandas as pd
-
+import numpy as np
 
 class DataHandler:
-    def __init__(self, hour, minute, sec):
-        pass
+    def __init__(self, start_time, end_time, instrument):
+        self.instrument = instrument
+        self.start_time = start_time
+        self.end_time = end_time
+        self.recorded = False
 
-    def __call__(self, *args, **kwargs):
-        raise NotImplementedError
+    def __call__(self, time, df):
+        if time == self.start_time and not self.recorded:
+            self._Compute()
+        elif time == self.end_time:
+            pass
+
+    def _Compute(self):
+        raise  NotImplementedError
 
 
 class MidPriceHandler(DataHandler):
 
-    def __init__(self, hour, minute, sec):
-        super().__init__()
+    def __init__(self, start_time, end_time, instrument):
+        super().__init__(start_time, end_time, instrument)
+        self.
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, time, df):
         pass
+
+    def _Compute(self):
 
 
 class OBPressureHandler(DataHandler):
-    def __init__(self):
-        pass
+    def __init__(self, start_time, end_time, instrument):
+        super().__init__(start_time, end_time, instrument)
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, time):
         pass
