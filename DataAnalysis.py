79942f2c4b7568337aa9fe4b7ca5ac988a594964
randysuen1991 <randysuen1991@gmail.com>
2018-10-15 12:18:25 +0800

    new

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,7 +1,17 @@
-from sklearn.decomposition import FactorAnalysis
+import numpy as np
+import pandas as pd
 
 
-class FactorModel():
-    def __init__(self, n_components, **kwargs):
-        self.fa = FactorAnalysis(n_components=n_components)
+class DataAnalysis:
 
+    # This method points out the data which are outliers, determined by the standard deviation in the group of data.
+    # input argument should be the data itself and the number which is going to be used to judge whether a datum is
+    # a outlier or not.
+    @staticmethod
+    def outlierhandler(x_train, num):
+        std = np.std(x_train)
+        mean = np.mean(x_train)
+        centered_x_train = x_train - mean
+        deviation = centered_x_train / std
+        index = np.where(deviation > num)
+        return index
